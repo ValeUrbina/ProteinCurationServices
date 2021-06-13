112766c05d49cb378baab769dcad33a5cf1543c7
Valeria Urbina <valeria.urbinac@pucp.edu.pe>
2021-06-13 05:27:18 -0500

    Resolved hmmr problem

--- hmmer.py
+++ hmmer.py
@@ -6,25 +6,27 @@ import to_fasta
 # Create a hmm and search the similar domains using hmmer
 # this file should be located in the directory specified by pfam_curation_tools docker
 
-SCRIPT = "./exec_hmmersearch.sh {} {} {} {}"
+SCRIPT1 = "hmmbuild {} {}"
+SCRIPT2 = "hmmsearch --tblout tblout.txt --domtblout domtblout.txt --pfamtblout pfamtblout.txt hmmALIGN {}"
 
 # file_name = file_name
 # outputfile_name = hmmALIGN, domtblout.txt
 
 
 def main(PATH, pfamseq_path, directory, pfam_code, file_name):
-    # Nos dirigimos a la carpeta donde se levantará el docker
-    os.chdir(PATH + '/Scripts')
-    # Se ejecuta hmmer
+    # Nos dirigimos a la carpeta donde se va a ejecutar hmmer
     project_path = os.path.join(
         PATH, 'pfam_data', directory, pfam_code)
+    os.chdir(project_path)
+    # Se ejecuta hmmer
     target_path = os.path.join(
         PATH, 'pfam_data', directory, pfam_code, 'hmmALIGN')
     source_path = os.path.join(
         PATH, 'pfam_data', directory, pfam_code, file_name)
-    my_cmd = SCRIPT.format(project_path, target_path,
-                           source_path, pfamseq_path)
-    os.system(my_cmd)
+    my_cmd_1 = SCRIPT1.format(target_path, source_path)
+    my_cmd_2 = SCRIPT2.format(pfamseq_path)
+    os.system(my_cmd_1)
+    os.system(my_cmd_2)
     dom_path = os.path.join(
         PATH, 'pfam_data', directory, pfam_code, 'domtblout.txt')
     # Se retorna la ruta del archivo
