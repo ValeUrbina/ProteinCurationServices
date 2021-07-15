7d297313d4cbd6bfe4eb071efde06bdf8225dd52
Valeria Urbina <valeria.urbinac@pucp.edu.pe>
2021-07-14 23:19:10 -0500

    Resolved delete right and left problems

--- deleteright.py
+++ deleteright.py
@@ -22,7 +22,7 @@ def main(PATH, directory, pfam_code, column, file_name, outputfile_name):
     for i, sequence in enumerate(sequences):
         simple_seq = sequence.split()
         index = sequence.find(simple_seq[1])
-        output_file.write(sequence[:index+column])
+        output_file.write(sequence[:index+column-1])
         output_file.write('\n')
 
     input_file.close()
